2187ce2ff384c97006ff136862319b2796c41d1f
Alexandr Adamov <wofff_4@mail.ru>
2020-10-04 11:16:37 +0300

    Updates

---
+++ precache-manifest.b26d9081264ff03aecebf15714a2dd0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18787d752cfb86a0734abcf2cfdec040",
+    "url": "//github.com/Alexandr-Adamov/Rambler-test/index.html"
+  },
+  {
+    "revision": "c323ea8e3eb995240952",
+    "url": "//github.com/Alexandr-Adamov/Rambler-test/static/css/main.ab4b87e7.chunk.css"
+  },
+  {
+    "revision": "dadb1c5a434089e84ca5",
+    "url": "//github.com/Alexandr-Adamov/Rambler-test/static/js/2.0f120c71.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "//github.com/Alexandr-Adamov/Rambler-test/static/js/2.0f120c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c323ea8e3eb995240952",
+    "url": "//github.com/Alexandr-Adamov/Rambler-test/static/js/main.1443bca8.chunk.js"
+  },
+  {
+    "revision": "289c28255ea3966c6f2c",
+    "url": "//github.com/Alexandr-Adamov/Rambler-test/static/js/runtime-main.9bc7b833.js"
+  }
+]);
